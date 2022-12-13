f9f9bb70e690d30b8c408fbf88f650e44646a42c
aleenaahmad1 <116086386+aleenaahmad1@users.noreply.github.com>
2022-12-13 11:53:49 +0500

    creating lexicon dictionary at the expense of my sanity

--- cleaning_dataset.py
+++ cleaning_dataset.py
@@ -1,55 +1,88 @@
 import json 
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk.corpus import wordnet
-from nltk.stem import WordNetLemmatizer
-from nltk.stem.snowball import SnowballStemmer 
+# import nltk
+# from nltk.corpus import stopwords
+# from nltk.tokenize import word_tokenize
+# from nltk.corpus import wordnet
+
+# from nltk.stem import WordNetLemmatizer
+# from nltk.stem.snowball import SnowballStemmer 
 
 
-file = open('pbs.json')
-data = json.load(file) #returns a list(of dictionaries?)
+data = open('final.txt')
+#data = json.load(file) #returns a list(of dictionaries?)
 
-tokens = [] #for tokenized words
-withoutStopWords = []
+# tokens = [] #for tokenized words
+# withoutStopWords = []
 
-lemm = WordNetLemmatizer()
+# lemm = WordNetLemmatizer()
 
-stop_words = stopwords.words('english') 
+# stop_words = stopwords.words('english') 
 
-for i in data:
-    tokens.append(word_tokenize(i["content"]))
-
-
-for word in tokens:
-    for j in word:
-        if (j.isalnum() and j.casefold() not in stop_words):
-            withoutStopWords.append(j)
+# for i in data:
+#     tokens.append(word_tokenize(i["content"]))
 
 
+# for word in tokens:
+#     for j in word:
+#         if (j.isalnum() and j.casefold() not in stop_words):
+#             withoutStopWords.append(j)
 
-withoutStopWords = nltk.pos_tag(withoutStopWords) #part of speech tags added
 
-finalWords=[]
 
-snow_stemmer = SnowballStemmer(language='english')
+# withoutStopWords = nltk.pos_tag(withoutStopWords) #part of speech tags added
 
-for word in withoutStopWords:
-    if(word[1].startswith('J')):
-        pos = wordnet.ADJ
-    elif(word[1].startswith('V')):
-        pos = wordnet.VERB
-    elif(word[1].startswith('R')):
-        pos = wordnet.ADV
-    else:
-        pos = wordnet.NOUN
-    finalWords.append(lemm.lemmatize(word[0], pos))
+# finalWords=[]
 
-file1 = open("final.txt", "a", encoding='utf-8')
+# for word in withoutStopWords:
+#     if(word[1].startswith('J')):
+#         pos = wordnet.ADJ
+#     elif(word[1].startswith('V')):
+#         pos = wordnet.VERB
+#     elif(word[1].startswith('R')):
+#         pos = wordnet.ADV
+#     else:
+#         pos = wordnet.NOUN
+#     finalWords.append(lemm.lemmatize(word[0], pos))
 
-for word in finalWords:
-    word=word.lower()
-    file1.write(word)
-    file1.write(" ")
+#final words: complete list of words in file
+
+lexicon = {}
+id=0
+
+with open('final.txt', 'r', encoding='utf-8') as file:
+    for line in file:
+        for word in line.split():
+            if(word in lexicon):
+                pass
+            else:
+                lexicon.update({word:id})
+                id+=1
+            
+
+# for i in finalWords:
+#     if(i in lexicon):
+#         pass
+#     else:
+#         lexicon.update({i:id})
+#         id+=1
+
+
+json_object = json.dumps(lexicon)
+
+with open("finallexicon.json", "w", encoding='utf-8') as finalfile:
+    finalfile.write(json_object)
+
+
+# file1 = open("final.txt", "a", encoding='utf-8')
+
+# for word in finalWords:
+#     word=word.lower()
+#     file1.write(word)
+#     file1.write(" ")
+
+
+    
+
+
 
 
